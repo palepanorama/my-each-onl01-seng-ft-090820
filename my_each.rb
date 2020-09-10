643856b0b4bd140f528df74cc6932c498f802a3c
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-10 22:53:36 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+
+def my_each(array)
+  i = 0 
+  while i < array.length 
+  yield(array[i])
+  i += 1 
+  end 
+  array 
 end
+
+
+
+def yo(array)
+  i = 0 
+  new_collection = []
+  while i < array.length 
+    new_collection << yield(array[i])
+    i += 1 
+  end
+  new_collection 
+end 
