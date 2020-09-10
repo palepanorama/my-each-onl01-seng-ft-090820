c362de0b91b671ff25e0763c6d1442a08d629056
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-10 22:45:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,15 @@
 collection = [1, 2, 3, 4]
 
+
+def my_each(array)
+  i = 0 
+  while i < array.length 
+  yield(array[i])
+  i += 1 
+  end 
+end
+
+
 def my_each(array)
   i = 0 
   collection = []
